0c87f063e3d8495c42f9a3d8e4c9e6702e8d140b
Reno Kingston <kingston@Renos-MacBook-Air.local>
2025-08-19 15:09:43 +0200

    Add logout button exit door

--- components/icons.tsx
+++ components/icons.tsx
@@ -4,12 +4,6 @@ export const ExitDoorIcon: React.FC<IconProps> = ({ className }) => (
         <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H9m0 0l3-3m-3 3l3 3" />
     </svg>
 );
-export const ExitDoorIcon: React.FC<IconProps> = ({ className }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6A2.25 2.25 0 005.25 5.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15" />
-        <path strokeLinecap="round" strokeLinejoin="round" d="M18 12H9m0 0l3-3m-3 3l3 3" />
-    </svg>
-);
 import React from 'react';
 
 type IconProps = {
