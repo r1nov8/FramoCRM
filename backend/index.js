b4093b762613cbf2a6bdc2f7e3845d8a4136dda0
Reno Kingston <kingston@Renos-MacBook-Air.local>
2025-08-19 14:26:11 +0200

    Update backend code

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import pkg from 'pg';
-const bcrypt = require('bcryptjs');
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const { Pool } = pkg;
